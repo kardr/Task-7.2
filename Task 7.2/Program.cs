d7abb89b812dbc368e271ee561253bbc4849c562
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-19 16:56:13 +0300

    .

--- Task 7.2/Program.cs
+++ Task 7.2/Program.cs
@@ -10,6 +10,16 @@ namespace Task_7._2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите строку символов: ");
+            string s = Convert.ToString(Console.ReadLine());
+            string prepin = " ,.!:;?-";
+            string[] razd = s.Split(prepin.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            Array.Sort(razd, (a, b) => a.Length - b.Length);
+            foreach (string w in razd)
+            {
+                Console.WriteLine(w);
+            }
+            Console.ReadKey();
         }
     }
 }
